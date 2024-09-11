a2e44bc28f97bbaaad32098528b57dafcb2e920a
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-09-11 18:21:12 -0400

    modified auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, flash
+import re
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,22 @@ def login():
 def logout():
     return "<p>Logout<p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if not re.match(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$', email):
+            flash('That\'s not a proper email address!', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than one character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least seven characters.', category='error')
+        else:
+            flash('Account created!', category='success')  
     return render_template("sign_up.html")
