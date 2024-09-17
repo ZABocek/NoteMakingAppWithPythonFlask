917900c804dffc0cfd80b38351d8dd500a873aee
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-09-17 15:29:26 -0400

    modified auth.py

--- website/auth.py
+++ website/auth.py
@@ -3,8 +3,43 @@ import re
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+import string
 
 auth = Blueprint('auth', __name__)
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+    
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+        
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+    
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = input(characters)
+        pwd += new_char
+        
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+            
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+            
+    return pwd
+    
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -42,8 +77,8 @@ def sign_up():
             flash('First name must be greater than one character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least seven characters.', category='error')
+        elif not generate_password(10):
+            flash('Password must be at least 10 characters and contain a special character and number.', category='error')
         else:
             new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
